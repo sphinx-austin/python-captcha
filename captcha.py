90c07fd27949e981aa726c70264e8e5ff5083a71
sphinx-austin <austinjuniour4@gmail.com>
2023-08-31 14:21:17 +0300

    generate & save

--- captcha.py
+++ captcha.py
@@ -6,3 +6,14 @@ def generate_captcha_text(length=6):
     import string
     import random
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+# generate and save
+def generate_and_save_captch(image_width=300,image_height=100,captcha_length=6,save_path='CAPCHA.png'):
+    image = ImageCaptcha(width=image_width, height=image_height)
+    captcha_text = generate_captcha_text(captcha_length)
+    data = Image.generate(captcha_text)
+    image.write(captcha_text, save_path)
+    return captcha_text
+
+    
